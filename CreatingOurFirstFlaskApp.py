2a9e43eacf5b7816d81c492c219fee518e03140e
Shreya Singh <singh.s.shreya2004@gmail.com>
2025-03-13 11:59:17 +0530

    Update CreatingOurFirstFlaskApp.py

--- CreatingOurFirstFlaskApp.py
+++ CreatingOurFirstFlaskApp.py
@@ -1,1 +1,40 @@
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+app.run()  #write this to run
+
+#to make different end points.
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/harry")
+def harry():
+    return "<p>Hello Harry!</p>"
+
+app.run()
+
+#write debug=True (this will update any change that has been done). 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/harry")
+def harry():
+    return "<p>Hello Harry4!</p>"
+
+app.run(debug=True)
